4fdcbf496cf550ba223085110318fdcd53990bc9
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-07-29 23:24:38 +0700

    Refine

--- src/main/java/com/example/hotelmanagement/role/model/repository/RoleRepository.java
+++ src/main/java/com/example/hotelmanagement/role/model/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.example.hotelmanagement.role.model.repository;
 
 import com.example.hotelmanagement.role.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
