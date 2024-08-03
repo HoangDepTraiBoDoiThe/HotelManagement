27d628a7310b47548d392735123400079dbb5bf9
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-03 12:58:36 +0700

    Refine model repositories

--- src/main/java/com/example/hotelmanagement/role/model/repository/RoleRepository.java
+++ src/main/java/com/example/hotelmanagement/role/model/repository/RoleRepository.java
@@ -4,6 +4,5 @@ import com.example.hotelmanagement.role.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/com/example/hotelmanagement/user/model/repository/UserRepository.java
+++ src/main/java/com/example/hotelmanagement/user/model/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package com.example.hotelmanagement.user.model.repository;
 
-import com.example.hotelmanagement.user.model.User;
+import com.example.hotelmanagement.user.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 }
