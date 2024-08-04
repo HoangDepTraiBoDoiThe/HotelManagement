668072693f0fa288325cc401e880382001824cf4
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-04 14:23:01 +0700

    Add reservation model

---
+++ src/main/java/com/example/hotelmanagement/reservation/model/Reservation.java
@@ -0,0 +1,26 @@
+package com.example.hotelmanagement.reservation.model;
+
+import com.example.hotelmanagement.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity(name = "reservations")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservation {
+    @Id
+    private long id;
+    private Date checkIn;
+    private Date checkOut;
+    private BigDecimal totalPrice;
+    
+    @ManyToOne
+    @JoinColumn(name = "Owner_Id")
+    private User owner;
+}
--- src/main/java/com/example/hotelmanagement/user/model/User.java
+++ src/main/java/com/example/hotelmanagement/user/model/User.java
@@ -1,12 +1,15 @@
 package com.example.hotelmanagement.user.model;
 
+import com.example.hotelmanagement.reservation.model.Reservation;
 import com.example.hotelmanagement.role.model.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Data
@@ -26,4 +29,7 @@ public class User {
     @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST}, fetch = FetchType.EAGER)
     @JoinTable(name = "UserRole", joinColumns = @JoinColumn(name = "userId", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "roleId", referencedColumnName = "Id"))
     private Set<Role> roles = new HashSet<>();
+    
+    @OneToMany(mappedBy = "owner")
+    private List<Reservation> reservations = new ArrayList<>();
 }
