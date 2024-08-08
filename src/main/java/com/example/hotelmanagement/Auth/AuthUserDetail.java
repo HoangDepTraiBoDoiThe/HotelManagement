4621d279a95adb8f0d87aefffaae3dc916839a5e
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-08 15:19:12 +0700

    Add AuthUserDetail model

---
+++ src/main/java/com/example/hotelmanagement/Auth/AuthUserDetail.java
@@ -0,0 +1,58 @@
+package com.example.hotelmanagement.Auth;
+
+import com.example.hotelmanagement.constants.ApplicationRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class AuthUserDetail implements UserDetails {
+
+    String userName;
+    String password;
+    String email;
+    List<ApplicationRole> owningApplicationRoles;
+    boolean isEnable = true;
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnable;
+    }
+}
