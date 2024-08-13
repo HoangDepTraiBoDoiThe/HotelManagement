08317988a713ee0692cd06974be2c3346b4b2e37
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-13 12:59:10 +0700

    Update room type CRUD - update and create

--- src/main/java/com/example/hotelmanagement/dto/request/RoomTypeRequest.java
+++ src/main/java/com/example/hotelmanagement/dto/request/RoomTypeRequest.java
@@ -3,11 +3,13 @@ package com.example.hotelmanagement.dto.request;
 import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public record RoomTypeRequest(
         @NotBlank(message = "Room type name is required")
         @Size(max = 50, message = "Room type name must be less than 50 characters")
         String roomTypeName,
+        List<Long> utilityIds,
         @NotBlank(message = "Description is required")
         @Size(max = 100, message = "Description must be less than 100 characters")
         String description,
--- src/main/java/com/example/hotelmanagement/service/RoomTypeService.java
+++ src/main/java/com/example/hotelmanagement/service/RoomTypeService.java
@@ -3,17 +3,22 @@ package com.example.hotelmanagement.service;
 import com.example.hotelmanagement.dto.request.RoomTypeRequest;
 import com.example.hotelmanagement.exception.ResourceNotFoundException;
 import com.example.hotelmanagement.model.RoomType;
+import com.example.hotelmanagement.model.Utility;
 import com.example.hotelmanagement.model.repository.RoomTypeRepository;
+import com.example.hotelmanagement.model.repository.UtilityRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class RoomTypeService {
     private final RoomTypeRepository roomTypeRepository;
+    private final UtilityRepository utilityRepository;
     
     public List<RoomType> getAllRoomTypes() {
         return roomTypeRepository.findAll();
@@ -26,6 +31,8 @@ public class RoomTypeService {
     @Transactional
     public RoomType createRoomType(RoomTypeRequest roomTypeRequest) {
         RoomType newRoomType = new RoomType(roomTypeRequest.roomTypeName(), roomTypeRequest.description(), roomTypeRequest.roomCapability(), roomTypeRequest.basePrice());
+        Set<Utility> utilities = new HashSet<>(utilityRepository.findAllById(roomTypeRequest.utilityIds()));
+        newRoomType.setRoomTypeUtilities(utilities);
         return roomTypeRepository.save(newRoomType);
     }
 
@@ -37,6 +44,8 @@ public class RoomTypeService {
         roomType.setDescription(roomTypeRequest.description());
         roomType.setBasePrice(roomTypeRequest.basePrice());
         roomType.setCapacity(roomTypeRequest.roomCapability());
+        Set<Utility> utilities = new HashSet<>(utilityRepository.findAllById(roomTypeRequest.utilityIds()));
+        roomType.setRoomTypeUtilities(utilities);
         // todo: room type images missing
         return roomTypeRepository.save(roomType);
     }
