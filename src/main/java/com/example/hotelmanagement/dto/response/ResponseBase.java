ff5d26e01dc4e5f8494581df153c0f47f03818ee
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-16 14:04:30 +0700

    feat(dto): add response DTOs
    Adds the following DTOs:
    
    - UtilityResponse_Minimal: a minimal DTO for utility information

---
+++ src/main/java/com/example/hotelmanagement/dto/response/ResponseBase.java
@@ -0,0 +1,12 @@
+package com.example.hotelmanagement.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseBase {
+    private long id;
+}
---
+++ src/main/java/com/example/hotelmanagement/dto/response/UtilityResponse_Minimal.java
@@ -0,0 +1,18 @@
+package com.example.hotelmanagement.dto.response;
+
+import com.example.hotelmanagement.model.Utility;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+public class UtilityResponse_Minimal extends ResponseBase {
+    private String utilityName;
+
+    public UtilityResponse_Minimal(Utility utility) {
+        super(utility.getId());
+        this.utilityName = utility.getUtilityName();
+    }
+}
